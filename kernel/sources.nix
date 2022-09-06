83bab1bebc836fb79f28b3c09620d204d64c7756
Erling Ljunggren <erlingljunggren@gmail.com>
2022-09-06 19:35:41 +0200

    update linux kernel

--- kernel/sources.nix
+++ kernel/sources.nix
@@ -1,9 +1,9 @@
 {fetchurl}:
 rec {
-  linux_version="linux-5.17";
+  linux_version="linux-5.19.7";
 
   linux = fetchurl {
     url = "https://mirrors.edge.kernel.org/pub/linux/kernel/v5.x/${linux_version}.tar.gz";
-    sha256 = "1n60y80p57hgsa65dbqmykw07ac9xyyxxcwwmd1rgiq9ngldb2ws";
+    sha256 = "0vqzr7v9zw4k34cf5wqzi96xgjzxwm36bmdjivhg8p5yfy5ml3cr";
   };
 }
